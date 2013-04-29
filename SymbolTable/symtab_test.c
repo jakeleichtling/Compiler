f653b9e4032ef0ab7dbcefeddd8a7e36bd7692f6
Jake Leichtling <jake.leichtling@gmail.com>
2013-04-28 23:12:55 -0400

    Further work on symtab_test.c

--- SymbolTable/symtab_test.c
+++ SymbolTable/symtab_test.c
@@ -54,21 +54,41 @@ int main()
 	symboltable t = create_symboltable();
     int level;
 
+    printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
+
     printf("1.) Add int x @ addr 1\n\n");
     symnode int_x = insert_into_symboltable(t, "x", identifier);
     set_node_vartype(int_x, inttype);
     set_node_addr(int_x, 1);
 
+    printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
+
     printf("2.) Enter new scope\n\n");
     enter_scope(t);
+
+    printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
 
     printf("3.) Add double x @ addr 2\n\n");
     symnode double_x = insert_into_symboltable(t, "x", identifier);
     set_node_vartype(double_x, doubletype);
     set_node_addr(double_x, 2);
 
+    printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
+
     printf("4.) Enter new scope\n\n");
     enter_scope(t);
+
+    printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
 
     printf("5.) Look up and print out id \"x\" --> double x @ level 1\n");
     print_node(lookup_in_symboltable(t, "x", &level, identifier));
@@ -78,14 +98,22 @@ int main()
     symnode sc_x = insert_into_symboltable(t, "x", stringconst);
     set_node_addr(sc_x, 3);
 
+    printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
+
     printf("7.) Look up and print out stringconst \"x\" --> stringconst x @ level 2\n");
     print_node(lookup_in_symboltable(t, "x", &level, stringconst));
     printf("@ level: %d\n\n", level);
 
-    printf("8.) Add stringconst x @ addr 3 (same name as before!)\n\n");
+    printf("8.) Add stringconst x (same name as (6)!)\n\n");
     insert_into_symboltable(t, "x", stringconst);
 
-    printf("9.) Look up and print out stringconst \"x\" --> stringconst x @ level 2 (same node as before!)\n");
+    printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
+
+    printf("9.) Look up and print out stringconst \"x\" --> stringconst x @ level 2 (same node as (7)!)\n");
     print_node(lookup_in_symboltable(t, "x", &level, stringconst));
     printf("@ level: %d\n\n", level);
 
@@ -98,22 +126,40 @@ int main()
     set_node_vartype(v_ftn_x, voidtype);
     set_node_addr(sc_x, 3);
 
+    printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
+
     printf("12.) Look up and print out id \"x\" --> void function x @ level 2\n");
     print_node(lookup_in_symboltable(t, "x", &level, identifier));
     printf("@ level: %d\n\n", level);
 
-    printf("13.) Print out the entire symbol table:\n------------------------------------\n");
-    print_symboltable(t);
+
+    printf("13.) Leave the current scope\n\n");
+    leave_scope(t);
+
     printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
 
-    printf("14.) Leave the current scope\n\n");
-
-    printf("15.) Look up and print out stringconst \"x\" --> null\n");
+    printf("14.) Look up and print out stringconst \"x\" --> null\n");
     print_node(lookup_in_symboltable(t, "x", &level, stringconst));
+    printf("\n");
+
+    printf("15.) Look up and print out id \"x\" --> double x @ level 1\n");
+    print_node(lookup_in_symboltable(t, "x", &level, identifier));
     printf("@ level: %d\n\n", level);
 
+    printf("16.) Leave the current scope\n\n");
+    leave_scope(t);
 
-    printf("17.) Leave the current scope\n\n");
+    printf("------------------------------------\n");
+    print_symboltable(t);
+    printf("------------------------------------\n\n");
+
+    printf("17.) Look up and print out id \"x\" --> int x @ level 0\n");
+    print_node(lookup_in_symboltable(t, "x", &level, identifier));
+    printf("@ level: %d\n\n", level);
 
 	destroy_symboltable(t);
 }
