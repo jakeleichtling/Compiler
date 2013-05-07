0207a05331114f4e3c9655d9fcc48539d996dbb3
Jake Leichtling <jake.leichtling@gmail.com>
2013-05-06 20:33:08 -0400

    Working on intermediate code generation

--- SemanticRoutines/quad.c
+++ SemanticRoutines/quad.c
@@ -1,7 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "quad.h"
 
+extern quad *quad_array;
+extern int next_quad_index;
+extern int quad_array_size;
+
+extern symboltable id_table;
+
+char *curr_func_name;
+int temp_count = 0;
+
+/* human readable quad operation names */
+char *quad_op_string[] = {
+  "call_func_op",
+  "print_int_op",
+  "print_float_op",
+  "print_string_op",
+  "int_to_float_op",
+  "assn_op"
+};
+
+void add_quad_to_array(quad new_quad);
+
+// Creates a quad and adds it to the quad array
 quad generate_quad(enum quad_op _op, quad_arg _arg1, quad_arg _arg2, quad_arg _arg3)
 {
   quad new_quad = (quad) calloc(1, sizeof(struct quad));
@@ -9,8 +32,11 @@ quad generate_quad(enum quad_op _op, quad_arg _arg1, quad_arg _arg2, quad_arg _a
   new_quad->arg1 = _arg1;
   new_quad->arg2 = _arg2;
   new_quad->arg3 = _arg3;
+
+  add_quad_to_array(new_quad);
 }
 
+// Retroactively changes an argument of a previously generated quad
 void patch_quad(quad q, int arg_index, quad_arg new_quad_arg)
 {
   switch (arg_index) {
@@ -25,5 +51,252 @@ void patch_quad(quad q, int arg_index, quad_arg new_quad_arg)
       break;
     default:
       fprintf(stderr, "patch_quad() requires an argument index between 1 and 3 (inclusive).");
+  }
+}
+
+// Creates a quad argument. Fields must be set manually
+quad_arg generate_quad_arg()
+{
+  quad_arg new_quad_arg = (quad_arg) calloc(1, sizeof(struct quad_arg));
+}
+
+// Set the temp prefix to the function name and reset the temp count at 1
+void set_temp_prefix(char *func_name)
+{
+  curr_func_name = strdup(func_name);
+  temp_count = 0;
+}
+
+// Get a new temp with the current function name as the prefix
+quad_arg get_new_temp(symboltable symtab, enum vartype var_type)
+{
+  int digits_counter_num = temp_count / 10;
+  int num_digits = 1;
+  while (digits_counter_num != 0) {
+    num_digits++;
+    digits_counter_num /= 10;
+  }
+
+  int basename_len = strlen(curr_func_name) + num_digits + 4;
+  char *basename = calloc(basename_len + 1, sizeof('a'));
+  snprintf(basename, basename_len + 1, "temp%d", temp_count);
+
+  // Make the symbol table node for the temp
+  symnode temp_symnode = insert_into_symboltable_with_prefix(symtab, basename, curr_func_name);
+  temp_symnode->var_type = var_type;
+  temp_symnode->node_type = val_node;
+
+  // Make the quad_arg for the temp, pointing to the symnode
+  quad_arg new_quad_arg = generate_quad_arg();
+  new_quad_arg->value.var_node = temp_symnode;
+  new_quad_arg->arg_type = id_arg;
+
+  temp_count++;
+  return new_quad_arg;
+}
+
+// Return right-side temps after assignment so that they can be reused
+void return_temps(int num_temps_returned)
+{
+  temp_count -= num_temps_returned;
+}
+
+// Recursive function for generating intermediate code using quads.
+//  If applicable, returns the quad_arg that holds the result of an operation
+quad_arg generate_intermediate_code(ast_node node)
+{
+  if (!node)
+    return;
+
+  quad_arg arg1, arg2, arg3;
+  arg1 = generate_quad_arg();
+  arg2 = generate_quad_arg();
+  arg3 = generate_quad_arg();    
+
+  switch (node->node_type) {
+    case ROOT:
+      generate_intermediate_code(node->left_child);
+      break;
+    case ID:
+      return;
+      break;
+    case INT_TYPE:
+      return;
+      break;
+    case DBL_TYPE:
+      return;
+      break;
+    case VOID_TYPE:
+      return;
+      break;
+    case ARRAY_SUB:
+      // TODO
+      break;
+    case ARRAY_NONSUB:
+      return;
+      break;
+    case OP_ASSIGN:
+//      quad_arg left_arg = 
+      break;
+    case OP_ADD:
+
+      break;
+    case OP_SUB:
+
+      break;
+    case OP_MULT:
+
+      break;
+    case OP_DIV:
+
+      break;
+    case OP_MOD:
+
+      break;
+    case OP_LT:
+
+      break;
+    case OP_LEQ:
+
+      break;
+    case OP_GT:
+
+      break;
+    case OP_GEQ:
+
+      break;
+    case OP_EQ:
+
+      break;
+    case OP_NEQ:
+
+      break;
+    case OP_AND:
+
+      break;
+    case OP_OR:
+
+      break;
+    case OP_BANG:
+
+      break;
+    case OP_NEG:
+
+      break;
+    case OP_INC:
+
+      break;
+    case OP_DEC:
+
+      break;
+    case FUNC_DECL:
+
+      break;
+    case VAR_DECL:
+
+      break;
+    case FORMAL_PARAM:
+
+      break;
+    case SEQ:
+
+      break;
+    case IF_STMT:
+
+      break;
+    case WHILE_LOOP:
+
+      break;
+    case DO_WHILE_LOOP:
+
+      break;
+    case FOR_STMT:
+
+      break;
+    case RETURN_STMT:
+
+      break;
+    case READ_STMT:
+
+      break;
+    case PRINT_STMT:
+      arg1->arg_type = id_arg;
+
+      if (node->left_child->node_type == STRING_LITERAL) {
+        arg1->value.var_node = node->left_child->value.sym_node;
+        generate_quad(print_string_op, arg1, NULL, NULL);
+      } else {
+        quad_arg expr_val = generate_intermediate_code(node->left_child);
+
+        if (node->left_child->data_type == inttype) {
+          generate_quad(print_int_op, expr_val, NULL, NULL);
+        } else if (node->left_child->data_type == doubletype) {
+          generate_quad(print_float_op, expr_val, NULL, NULL);
+        }
+      }
+
+      break;
+    case STRING_LITERAL:
+      return;
+      break;
+    case INT_LITERAL:
+      return;
+      break;
+    case DOUBLE_LITERAL:
+      return;
+      break;
+    case FUNC_CALL:
+      arg1->arg_type = id_arg;
+      generate_quad(call_func_op, arg1, NULL, NULL);
+      break;
+    case EMPTY_EXPR:
+      return;
+      break;
+  }
+}
+
+// Add a quad to the array
+void add_quad_to_array(quad new_quad)
+{
+  if (next_quad_index >= quad_array_size) {
+    fprintf(stderr, "The quad array has been filled!\n");
+    exit(1);
+  }
+}
+
+// Prints the quad array in human-readable format
+void print_quad_array()
+{
+  int i;
+  for (i = 0; i < next_quad_index && i < quad_array_size; i++)
+    print_quad(quad_array[i]);
+}
+
+// Prints the quad in human-readable format
+void print_quad(quad the_quad)
+{
+  printf("(%s, ", quad_op_string[the_quad->op]);
+  print_quad_arg(the_quad->arg1);
+  print_quad_arg(the_quad->arg2);
+  print_quad_arg(the_quad->arg3);
+  printf("\n");
+}
+
+// Prints the quad_arg in human-readable format
+void print_quad_arg(quad_arg the_quad_arg)
+{
+  switch (the_quad_arg->arg_type) {
+    case int_arg:
+      printf("int: %d", the_quad_arg->value.int_value);
+      break;
+    case dbl_arg:
+      printf("double: %f", the_quad_arg->value.double_value);
+      break;
+    case id_arg:
+      printf("ID: %s", the_quad_arg->value.var_node->name);
+      break;
+    default:
+      printf("arg type not specified");
+      break;
   }
 }
--- SemanticRoutines/quad.h
+++ SemanticRoutines/quad.h
@@ -6,20 +6,33 @@
 
 /* quad operations */
 enum quad_op {
+  call_func_op,
+  print_int_op,
+  print_float_op,
+  print_string_op,
+  int_to_float_op,
+  assn_op,
+
   read_op,
   geq_op,
   ifF_op,
-  assn_op,
   lab_op,
   gt_op,
   mult_op,
   sub_op,
   goto_op,
-  print_op,
   halt_op,
   enter_scope_op,
   leave_scope_op,
   new_var_op
+};
+
+/* types of quad args */
+enum quad_arg_type {
+  no_arg_type,
+  int_arg,
+  dbl_arg,
+  id_arg
 };
 
 typedef struct quad_arg *quad_arg;
@@ -27,9 +40,9 @@ struct quad_arg {
   union {
     int int_value;
     double double_value;
-    ast_node var_node;
+    symnode var_node;
   } value;
-  enum vartype var_type;
+  enum quad_arg_type arg_type;
 };
 
 typedef struct quad *quad;
@@ -41,8 +54,35 @@ struct quad {
   quad_arg arg3;
 };
 
+// Creates a quad and adds it to the quad array
 quad generate_quad(enum quad_op, quad_arg, quad_arg, quad_arg);
 
+// Retroactively changes an argument of a previously generated quad
 void patch_quad(quad, int arg_index, quad_arg new_quad_arg);
+
+// Creates a quad argument. Fields must be set manually
+quad_arg generate_quad_arg();
+
+// Set the temp prefix to the function name and reset the temp count at 1
+void set_temp_prefix(char *func_name);
+
+// Get a new temp with the current function name as the prefix
+quad_arg get_new_temp(symboltable symtab, enum vartype var_type);
+
+// Return right-side temps after assignment so that they can be reused
+void return_temps(int num_temps_returned);
+
+// Recursive function for generating intermediate code using quads.
+//  If applicable, returns the quad_arg that holds the result of an operation
+quad_arg generate_intermediate_code(ast_node node);
+
+// Prints the quad array in human-readable format
+void print_quad_array();
+
+// Prints the quad in human-readable format
+void print_quad(quad the_quad);
+
+// Prints the quad_arg in human-readable format
+void print_quad_arg(quad_arg the_quad_arg);
 
 #endif
