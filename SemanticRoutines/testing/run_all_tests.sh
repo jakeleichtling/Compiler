385cb2d19be3b47102f55c89fac9fbd9f99ec57f
Derek Salama <dereksalama@gmail.com>
2013-05-27 22:15:20 -0400

    file renaming and readmes

--- SemanticRoutines/testing/run_all_tests.sh
+++ SemanticRoutines/testing/run_all_tests.sh
@@ -2,7 +2,7 @@
 
 # Name: run_all_tests.sh
 #
-# Purpose: A bash script for running the Parser on the text of all test input files and saving the output in corresponding test output files.
+# Compile all .c57 test files
 #
 # Derek Salama & Jake Leichtling
 # CS57
@@ -16,13 +16,28 @@ cd ./testing
 
 echo "---------------------------------------------------------"
 
-echo "~~~~~~~~~~~~~~~~~~~ bellman-ford.c57.in ~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~ bellman-ford.c57 ~~~~~~~~~~~~~~~~~"
 ../djcc bellman-ford.c57 bellman-ford.tm57
 
-# echo "~~~~~~~~~~~~~~~~~~~ dijkstra.c57.in ~~~~~~~~~~~~~~~~~~~~~"
-#../djcc dijkstra.c57.in dijkstra.tm57
-#$TM57_PATH dijkstra.tm57 > dijkstra.output
+echo "~~~~~~~~~~~~~~~~~~~ dijkstra.c57 ~~~~~~~~~~~~~~~~~~~~~"
+../djcc dijkstra.c57 dijkstra.tm57
 
-# echo "~~~~~~~~~~~~~~ binary_operations.c57.in ~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~ merge_sort.c57 ~~~~~~~~~~~~~~~~~~~~~"
+../djcc merge_sort.c57 merge_sort.tm57
+
+echo "~~~~~~~~~~~~~~~~~~~ decl_test.c57 ~~~~~~~~~~~~~~~~~~~~~"
+../djcc decl_test.c57 decl_test.tm57
+
+echo "~~~~~~~~~~~~~~~~~~~ op_test.c57 ~~~~~~~~~~~~~~~~~~~~~"
+../djcc op_test.c57 op_test.tm57
+
+echo "~~~~~~~~~~~~~~~~~~~ misc_test.c57 ~~~~~~~~~~~~~~~~~~~~~"
+../djcc misc_test.c57 misc_test.tm57
+
+echo "~~~~~~~~~~~~~~~~~~~ function_call_test.c57 ~~~~~~~~~~~~~~~~~~~~~"
+../djcc function_call_test.c57 function_call_test.tm57
+
+echo "~~~~~~~~~~~~~~~~~~~ variable_size_array_test.c57 ~~~~~~~~~~~~~~~~~~~~~"
+../djcc variable_size_array_test.c57 variable_size_array_test.tm57
 
 echo "---------------------------------------------------------"
